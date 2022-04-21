8045fa08eb7c2361b777cc573271f89587da1be7
JonathanCline <jonathancline64@gmail.com>
2022-04-21 02:22:49 -0500

    Added the initial files

---
+++ episode-1/build.sh
@@ -0,0 +1,4 @@
+#!
+
+mkdir _build
+clang++ -o _build/my_project.exe main.cpp
---
+++ episode-1/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "I am good at programming - I swear\n";
+	return 0;
+};
